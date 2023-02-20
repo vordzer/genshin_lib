7de51f36dbb5f615b30d1c46fe7895b1e1196ce1
Alexey Vorobyev <vordzer@gmail.com>
2023-02-21 01:40:04 +0300

    add Jean

--- pers_creator/main.cpp
+++ pers_creator/main.cpp
@@ -1,12 +1,13 @@
 #include<iostream>
 #include<fstream>
+#include<filesystem>
 #include<nlohmann/json.hpp>
 
-int main()
+void make_character(const std::string &fname)
 {
-    std::ifstream f("data/json/diluc.json");
+    std::ifstream f(fname);
     nlohmann::json data = nlohmann::json::parse(f);
-    std::cout << data["name"] << std::endl;
+    std::cout << "Make character: " << data["name"].get<std::string>() << std::endl;
     std::ofstream fout;
     fout.open("../src_lib/pers/diluc.h");
     fout << "#include<headers/g_lib.h>" << std::endl;
@@ -16,9 +17,16 @@ int main()
     fout << "class diluc : public character" << std::endl;
     fout << "{" << std::endl;
     fout << "public:" << std::endl;
-    fout << "diluc() : character(" << data["name"] << ", element::pyro, " << data["title"] << ") {}" << std::endl;
+    fout << "diluc() : character(" << data["name"] << ", element::" << data["element"].get<std::string>() << ", " << data["title"] << ") {}" << std::endl;
     fout << "};" << std::endl;
     fout << "}" << std::endl;
     fout.close();
+}
+
+int main()
+{
+    std::string path = "data/json/";
+    for (const auto &entry : std::filesystem::directory_iterator(path))
+        make_character(entry.path());
     return 0;
 }
